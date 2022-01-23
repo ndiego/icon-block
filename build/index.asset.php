aa0e0332d60b39b56d916d3242082ab445be3c0b
Nick Diego <ndiego@outermost.co>
2022-01-22 22:10:32 -0500

    Remove dev files and update build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '4c81bd499bed36daffcad40700f52ca6');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '8c2098bc221070f61ee108c1148b6776');
