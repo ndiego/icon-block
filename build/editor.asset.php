89851ecff13ffd4a8849d5cbfd5be08e83ff1585
Nick Diego <ndiego@outermost.co>
2022-10-17 14:13:11 -0500

    Fresh build.

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7433b970450c3b40e3ca');
+<?php return array('dependencies' => array(), 'version' => '30cd1c5e7b594ade5702');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '4e94e149ff1e32dee417');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '4d129138935c7405bcbe');
