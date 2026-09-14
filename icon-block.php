658b2d43298213d23be06780e4eb84f41682ba38
Nick Diego <nickmdiego@gmail.com>
2026-09-14 15:21:14 -0500

    Bump version to 2.1.0

--- icon-block.php
+++ icon-block.php
@@ -4,7 +4,7 @@
  * Description:       Easily add SVG icons and graphics to the WordPress block editor.
  * Requires at least: 6.6
  * Requires PHP:      7.4
- * Version:           2.0.0
+ * Version:           2.1.0
  * Author:            Nick Diego
  * Author URI:        https://www.nickdiego.com
  * License:           GPL-2.0-or-later
